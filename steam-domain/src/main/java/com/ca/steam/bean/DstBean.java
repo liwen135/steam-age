51ce69419cc296becd386ef7033b2ee9f8ca66da
liwen135 <1396968962@qq.com>
2018-05-01 00:27:07 +0800

    增加mybatis文件

---
+++ steam-domain/src/main/java/com/ca/steam/bean/DstBean.java
@@ -0,0 +1,44 @@
+package com.ca.steam.bean;
+
+public class DstBean {
+
+    private Integer age;
+
+    private String title;
+
+    private Boolean eat;
+
+    private String self;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getEat() {
+        return eat;
+    }
+
+    public void setEat(Boolean eat) {
+        this.eat = eat;
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
+    }
+}
---
+++ steam-domain/src/main/java/com/ca/steam/bean/SourceBean.java
@@ -0,0 +1,44 @@
+package com.ca.steam.bean;
+
+public class SourceBean {
+
+    private Integer age;
+
+    private String title;
+
+    private String source;
+
+    private Boolean eat;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Boolean getEat() {
+        return eat;
+    }
+
+    public void setEat(Boolean eat) {
+        this.eat = eat;
+    }
+}
